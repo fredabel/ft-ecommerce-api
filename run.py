0fdcbc2c84388895e4deac40caf7be05f7c4bfaf
fredabel <tuazonpred@gmail.com>
2025-07-30 19:26:41 -0400

    Put frontend link in cors

--- run.py
+++ run.py
@@ -4,7 +4,14 @@ from flask import redirect
 from flask_cors import CORS
 
 app = create_app('ProductionConfig')
-CORS(app)
+CORS(
+    app,
+    resources={r"/*": {"origins": [
+        "https://ct-service-ticket-client-production.up.railway.app"
+    ]}},
+    supports_credentials=True,
+    allow_headers=["Content-Type", "Authorization"]
+)
 @app.route('/', methods=['GET'])
 def index():
     return redirect('/api/docs')
